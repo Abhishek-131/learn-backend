fdb409c9caf74617dfc2257403692a5dfa786a10
Abhishek Kumar <113997298+Abhishek-131@users.noreply.github.com>
2026-08-29 19:56:16 +0530

    add login and registration (#1)
    Co-authored-by: Abhishek Kumar <abhishek.kumar3@trayistats.com>

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ dotenv.config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db')
 
-connectDB();
-
 const PORT = process.env.PORT || 5000;
+connectDB();
+console.log("Hello",process.env.JWT_SECRET);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
