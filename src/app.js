1fdb994e6d243abb6a4f3f87f7b1d5225d6248b6
Abhishek Kumar <113997298+Abhishek-131@users.noreply.github.com>
2026-09-04 00:15:30 +0530

    feat: RateLimiter and Security (#6)
    Co-authored-by: Abhishek Kumar <abhishek.kumar3@trayistats.com>

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const path = require('path');
 
 const userRoutes = require('./routes/userRoutes');
@@ -8,13 +10,37 @@ const errorHandler = require('./middleware/errorMiddleware')
 
 const app = express();
 
+//security headers
+app.use(helmet());
+
 //Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+        origin:process.env.FRONTEND_URL,
+        methods:['GET','POST','PUT','PATCH','DELETE'],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+//Limit JSON payload size
+app.use(express.json({limit:"10kb"}));
+
+//Serve Upload file
 app.use(
     "/uploads",
     express.static(path.join(__dirname,"../uploads"))
 );
+
+//Gloabal API rate limiter
+const apiLimiter = rateLimit({
+    windowMs: 15*60*1000,
+    limit: 100,
+    standardHeaders : true,
+    legacyHeaders :false,
+    message:{
+        success:false,
+        message:'Too many requests. Please try again later.'
+    }
+});
 
 //Test Routes
 app.get("/",(req,res)=>{
@@ -24,16 +50,17 @@ app.get("/",(req,res)=>{
     });
 });
 
+app.use('/api',apiLimiter);
+
 //user-routes
 app.use("/api/users",userRoutes);
 app.use("/api/auth",authRoutes);
 
 //404  routes
-app.use((req,res)=>{
-    rse.status(404).json({
-        success:false,
-        message:"404 NOT FOUND...",
-    });
+app.use((req,res,next)=>{
+    const error = new Error(`Route not found: ${req.originalUrl}`);
+    error.statusCode = 404;
+    next(error);
 });
 
 //Error Middleware
